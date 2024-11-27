a873c6cc9ae3ac7ff82712a688a8b5124f02ef37
Mariam Diasamidze <diasamidzemariam2101@gmail.com>
2024-11-27 15:24:49 +0400

    Dog's human age

---
+++ Day-2-steps/script.js
@@ -0,0 +1,9 @@
+//--> Numbers
+//How old would a dog be if it were a human of its age?
+
+//1.Ask the dog's age.
+let dogAge = prompt('How old is your dog?');
+//2.The formula for calculating a person's age based on a dog's age
+let humanAge = (dogAge - 2) * 4 + 21;
+//3.Showed the result
+console.log(humanAge)
