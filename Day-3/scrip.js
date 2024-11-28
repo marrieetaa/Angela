84a78a36b021ac8d0ec853a4964ebc9b3f6bea03
Mariam Diasamidze <diasamidzemariam2101@gmail.com>
2024-11-28 17:04:19 +0400

    Love calculator improvements

--- Day-3/scrip.js
+++ Day-3/scrip.js
@@ -1,7 +1,14 @@
 //--> Functions
 //-- Love Clculator
 function loveCalc (pers1,pers2){
-   let persent = Math.round((Math.random() * 100) + 1);
-   return `The chance of love is ${persent}%.`
+   let lovScore = Math.round((Math.random() * 100) + 1);
+   if(lovScore >= 70){
+      return `Your love scroe is ${lovScore}.You love each other like Kanye loves Kanye!`
+   }else if(lovScore < 70 && lovScore >= 30){
+      return `Your love scroe is ${lovScore}. There’s a spark between you that just needs to be ignited.`
+   }else{
+      return `You love score is ${lovScore}. You go together like oil and water.`
+   }
+  
 }
 console.log(loveCalc('Santiago','Peralta'));
