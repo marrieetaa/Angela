b5319804e0ad31515534415d90c743444c51bfab
Mariam Diasamidze <diasamidzemariam2101@gmail.com>
2024-11-28 15:35:46 +0400

    Add Love Calculator

---
+++ Day-3/scrip.js
@@ -0,0 +1,7 @@
+//--> Functions
+//-- Love Clculator
+function loveCalc (pers1,pers2){
+   let persent = Math.round((Math.random() * 100) + 1);
+   return `The chance of love is ${persent}%.`
+}
+console.log(loveCalc('Santiago','Peralta'));
