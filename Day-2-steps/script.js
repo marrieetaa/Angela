ab55f0134bce38a90e4d9aef383526aec94869a4
Mariam Diasamidze <diasamidzemariam2101@gmail.com>
2024-11-28 13:44:12 +0400

    Dynamic calculation added to milk purchase function

--- Day-2-steps/script.js
+++ Day-2-steps/script.js
@@ -11,16 +11,31 @@
 
 //--> Function
 //-- How many bottles of milk can be purchased with a given amount of money?
-function getMilk (money,costOfBottle){
-  let numberOfBottles = Math.floor(money / costOfBottle);
-  return  `Buy ${numberOfBottles} bottles of milk with the given amount.`;
+// function getMilk (money,costPerBottle){
+//   console.log('Buy ' + calcBottles(money, costPerBottle) + ' bottles of milk.')
+//  return calcChange(money,costPerBottle);
+// }
+
+// console.log(getMilk(10, 1.5));
+
+function getMilk (money,costPerBottle){
+ return `With the given amount, you can buy ${calcBottles(money, costPerBottle)} bottles of milk and the change will be ${calcChange(money,costPerBottle)}$.`
 }
 
-//console.log(getMilk(20,1.5));
+console.log(getMilk(10, 1.5));
+
+function calcBottles(startingMoney,costPerBottle){
+  //Number of bottles
+  return Math.floor(startingMoney / costPerBottle);
+}
+function calcChange (staringMoney,costPerBottle){
+  //Change
+  return staringMoney % costPerBottle
+}
 
 //-- How many days, weeks and months we have left if we live until 90 years old.
 function lifeInWeeks (age) {
   let yearsRemaining = 90 - age;
   return `You have ${yearsRemaining * 365} days, ${yearsRemaining * 52} weeks, and ${yearsRemaining * 12} months left.`
 }
-console.log(lifeInWeeks(27));
+//console.log(lifeInWeeks(27));
