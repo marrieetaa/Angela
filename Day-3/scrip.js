99946073f416b58de5e85bc224c87c8f2aaee127
Mariam Diasamidze <diasamidzemariam2101@gmail.com>
2024-11-28 19:09:49 +0400

    FizzBuzz

--- Day-3/scrip.js
+++ Day-3/scrip.js
@@ -41,4 +41,26 @@ function party (guest){
     return 'Sorry, maybe next time!'
   }
 }
-console.log(party());
+//console.log(party());
+
+
+//FizzBuzz
+let output = [];
+let number = 1;
+function fizzBuzz (){
+  if(number % 3 === 0 && number % 5 ===0){
+     output.push('FizzBuzz');
+  }else if(number % 3 === 0){
+     output.push('Fizz');
+  }else if(number % 5 === 0){
+     output.push('Buzz');
+  }else{
+    output.push(number);
+  }
+  number++
+  console.log(output);
+}
+
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
