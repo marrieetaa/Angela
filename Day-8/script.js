9da4ea315b925c8c937392bb6ffffdcb3de90d6a
Mariam Diasamidze <diasamidzemariam2101@gmail.com>
2025-01-14 01:08:05 -0800

    random chosen colour

--- Day-8/script.js
+++ Day-8/script.js
@@ -1,7 +1,11 @@
+const gamePattern = [];
+
 const buttonColours = ['red','blue','green','yellow'];
 
 const nextSequence = () => Math.floor(Math.random() * 4);
 const randomNumber = nextSequence();
 
 const randomChosenColour = buttonColours[randomNumber];
-console.log(randomChosenColour);
+gamePattern.push(randomChosenColour);
+
+
