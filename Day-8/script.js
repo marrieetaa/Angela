18f6f0d137e41a13850a0fa5b3a8f1bcbc271eac
Mariam Diasamidze <diasamidzemariam2101@gmail.com>
2025-01-14 01:51:08 -0800

    Add audio (data-)'

--- Day-8/script.js
+++ Day-8/script.js
@@ -12,3 +12,8 @@ $('.btn').on('click', function(){
   $(this).fadeOut().fadeIn();
 })
 
+$('.btn').on('click' , function (){
+    let soundFile = $(this).data('sound');
+    const sound = new Audio(soundFile);
+    sound.play();
+})
