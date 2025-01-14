8c3f12da996049a754346242db390225b2c1a1fb
Mariam Diasamidze <diasamidzemariam2101@gmail.com>
2025-01-13 22:51:46 -0800

    choose randomm color with random number'

--- Day-8/script.js
+++ Day-8/script.js
@@ -1,2 +1,7 @@
+const buttonColours = ['red','blue','green','yellow'];
+
 const nextSequence = () => Math.floor(Math.random() * 4);
-//console.log(nextSequence())
+const randomNumber = nextSequence();
+
+const randomChosenColour = buttonColours[randomNumber];
+console.log(randomChosenColour);
