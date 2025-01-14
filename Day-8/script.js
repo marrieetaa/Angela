6db9feea63897af486ba0c300df7c0fc29471ae4
Mariam Diasamidze <diasamidzemariam2101@gmail.com>
2025-01-14 07:20:30 -0800

    ....

--- Day-8/script.js
+++ Day-8/script.js
@@ -1,19 +1,36 @@
+//const userClickPattern 
+
 const gamePattern = [];
 
 const buttonColours = ['red','blue','green','yellow'];
-
-const nextSequence = () => Math.floor(Math.random() * 4);
-const randomNumber = nextSequence();
-
-const randomChosenColour = buttonColours[randomNumber];
-gamePattern.push(randomChosenColour);
+let level = 0;
+function nextSequence () { 
+  const randomNumber = Math.floor(Math.random() * 4);
+  const randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  level++
+};
 
 $('.btn').on('click', function(){
-  $(this).fadeOut().fadeIn();
-})
-
-$('.btn').on('click' , function (){
-    let soundFile = $(this).data('sound');
+  $(this).fadeIn(100).fadeOut(100).fadeIn(100).addClass('pressed');
+  setTimeout(()=>{
+    $(this).removeClass('pressed');
+  },100);
+  
+  let soundFile = $(this).data('sound');
     const sound = new Audio(soundFile);
     sound.play();
+})
+
+// $('.btn').on('click' , function (){
+//     let soundFile = $(this).data('sound');
+//     const sound = new Audio(soundFile);
+//     sound.play();
+// })
+
+$('.btn').on('click',nextSequence);
+console.log(nextSequence)
+
+$('.btn').on('click', function(){
+    $('h1').text('Level 0');
 })
