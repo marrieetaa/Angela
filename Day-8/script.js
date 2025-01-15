a505e2e92e99eefe305858bd750447b9a322eb91
Mariam Diasamidze <diasamidzemariam2101@gmail.com>
2025-01-15 01:25:12 -0800

    To be continued..

--- Day-8/script.js
+++ Day-8/script.js
@@ -1,21 +1,3 @@
-//const userClickPattern 
-
-//
-const gamePattern = [];
-
-//Colours of buttons
-const buttonColours = ['red','blue','green','yellow'];
-
-let level = 0;
-//
-function nextSequence () { 
-  const randomNumber = Math.floor(Math.random() * 4);
-  const randomChosenColour = buttonColours[randomNumber];
-  gamePattern.push(randomChosenColour);
-  $('h1').text(`level ${level++}`)
-};
-
-
 //just sounds and styles..
 $('.btn').on('click', function(){
   $(this).fadeIn(100).fadeOut(100).fadeIn(100).addClass('pressed');
@@ -28,11 +10,59 @@ $('.btn').on('click', function(){
     sound.play();
 })
 
-$('.btn').on('click',nextSequence);
+
+//
+const gamePattern = [];
+const userClickedPattern = [];
+//Colours of buttons
+const buttonColours = ['red','blue','green','yellow'];
+
+let level = 0;
+//random number, random color..
+function nextSequence () { 
+  const randomNumber = Math.floor(Math.random() * 4);
+  const randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  //increase the level..
+  $('h1').text(`level ${level++}`);
+
+};
 
 
+$('body').on('click',nextSequence);
+
+// let started = false;
+// $(document).keypress(function() {
+//   if (!started) {
+//     $("#level-title").text("Level " + level);
+//     nextSequence();
+//     started = true;
+//   }
+// });
 
 
 // function checkAnswer(currentLevel){
-    
+//   if(userClickedPattern === gamePattern){
+//     const p = document.createElement('p');
+//     p.textContent = 'Success';
+//     $('h1').after(p);
+//     $('p').css({
+//       'color': 'pink',
+//       'fontSize': '4rem',
+//       'transition': 'transform 0.5 ease',
+//       'transform': 'scaleX(1.8)'
+//     })  
+//     }else{
+//       const p = document.createElement('p');
+//       p.textContent = 'Wrong';
+//       $('h1').after(p);
+//       $('p').css({
+//         'color': 'red',
+//         'fontSize': '4rem',
+//         'transition': 'transform 0.5 ease',
+//         'transform': 'scaleX(1.8)'
+//     })
 // }
+// }
+
+//checkAnswer()
