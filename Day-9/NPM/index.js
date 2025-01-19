4f93ca9f801ca3727ffcc0c6ffc126a1fe15e7b9
Mariam Diasamidze <diasamidzemariam2101@gmail.com>
2025-01-18 22:04:32 -0800

    sillyName , superHero'

---
+++ Day-9/NPM/index.js
@@ -0,0 +1,19 @@
+//var generateName = require('sillyname');
+
+// import generateName from "sillyname";
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}`);
+
+import superheroes from "superheroes";
+//superheroes;
+console.log(`I am ${superheroes}`);
+
+import {randomSuperhero} from 'superheroes';
+
+randomSuperhero();
+console.log(`I am ${randomSuperhero}`);
+
+// import superheroes from "superheroes";
+// const name = superheroes.random();
+// console.log(`I am ${name}`);
